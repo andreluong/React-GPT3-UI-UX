e6f2ba77d6dbf1fec4a7dd5154100f44c06ad45e
Andre Luong <andreluong1@gmail.com>
2022-06-27 19:41:12 -0700

    create blog section

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-
+import { Article } from '../../components'
+import { blog01, blog02, blog03, blog04, blog05 } from '../../containers/blog/imports'
 import './blog.css'
 
 const Blog = () => {
@@ -10,10 +11,13 @@ const Blog = () => {
       </div>
       <div className="gpt3__blog-container">
         <div className="gpt3__blog-container_groupA">
-          
+          <Article imgUrl={blog01} date="Sept 26, 2021" title="GPT-3 and Open AI is the future. Let us explore how it is?"/>
         </div>
         <div className="gpt3__blog-container_groupB">
-          
+          <Article imgUrl={blog02} date="Sept 26, 2021" title="GPT-3 and Open AI is the future. Let us explore how it is?"/>
+          <Article imgUrl={blog03} date="Sept 26, 2021" title="GPT-3 and Open AI is the future. Let us explore how it is?"/>
+          <Article imgUrl={blog04} date="Sept 26, 2021" title="GPT-3 and Open AI is the future. Let us explore how it is?"/>
+          <Article imgUrl={blog05} date="Sept 26, 2021" title="GPT-3 and Open AI is the future. Let us explore how it is?"/>
         </div>
       </div>
     </div>
